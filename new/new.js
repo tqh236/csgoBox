bb6587d2d736b8d4aaf8d18a69660cc8638e3eba
PJ <552412@qq.com>
2023-10-31 22:39:56 +0800

    new111

--- new/new.js
+++ new/new.js
@@ -2,18 +2,6 @@ const obb = require('./a_new.json');
 let obj = obb.data 
 
 
-// obj.game_arena_player.forEach((e,i)=>{
-//   //席位 i
-//   // console.log(`席位${i+1}`);
-//   e.game_award.forEach((e2,i2)=>{
-//     // 回合 (箱子)
-//     // console.log(e);
-//     let find = obj.game_arena_box[0].box.contains.find(i => i.skin_id == e2.award_id)
-//     if(find){
-//       console.log(`席位${e.seat + 1}>>回合${i2 + 1}>>价值:${find.skins.bean}`);
-//     }
-//   })
-// })
 obj.game_arena_player.sort((a, b) => a.seat - b.seat);
 obj.game_arena_player.forEach((e,i)=>{
   //席位 i
@@ -21,10 +9,12 @@ obj.game_arena_player.forEach((e,i)=>{
   e.game_award.forEach((e2,i2)=>{
     // 回合 (箱子)
     // console.log(e);
+    let conut = 0
      obj.game_arena_box.forEach((b,bi)=>{
        let find = b.box.contains.find(i => i.skin_id == e2.award_id)
        if(find){
-         console.log(`席位${e.seat + 1}>>回合${i2 + 1}>>价值:${find.skins.bean}`);
+         conut += Number(find.skins.bean)
+         console.log(`席位${e.seat + 1}>>回合${i2 + 1}>>价值:${find.skins.bean} ${i2 + 1 == e.game_award.length ? '\xa0\xa0>>>席位' + (e.seat + 1) + '总价值：' + conut +'\n' :'' }`);
        }
     })
   })
