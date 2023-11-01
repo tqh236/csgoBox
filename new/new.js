8edabb2e298fb093c7cdc0f7edb3d6e7332a22c7
PJ <552412@qq.com>
2023-11-01 08:48:22 +0800

    new bug解决版

--- new/new.js
+++ new/new.js
@@ -4,19 +4,16 @@ let obj = obb.data
 
 obj.game_arena_player.sort((a, b) => a.seat - b.seat);
 obj.game_arena_player.forEach((e,i)=>{
-  //席位 i
-  // console.log(`席位${i+1}`);
+     // 席位(用户)
+  let count =0
   e.game_award.forEach((e2,i2)=>{
-    // 回合 (箱子)
-    // console.log(e);
-    let conut = 0
-     obj.game_arena_box.forEach((b,bi)=>{
-       let find = b.box.contains.find(i => i.skin_id == e2.award_id)
-       if(find){
-         conut += Number(find.skins.bean)
-         console.log(`席位${e.seat + 1}>>回合${i2 + 1}>>价值:${find.skins.bean} ${i2 + 1 == e.game_award.length ? '\xa0\xa0>>>席位' + (e.seat + 1) + '总价值：' + conut +'\n' :'' }`);
-       }
-    })
+       // 回合(箱子)
+    let findBoxObj = obj.game_arena_box.find(b => b.box_id == e2.box_id)
+    let findPrice = findBoxObj.box.contains.find(p => p.skin_id == e2.award_id)
+    if (findPrice){
+      count += Number(findPrice.skins.bean)
+      console.log(`席位${e.seat + 1}>>回合${i2 + 1}>>价值:${findPrice.skins.bean} ${i2 + 1 == e.game_award.length ? '\xa0\xa0>>>席位' + (e.seat + 1) + '总价值：' + count + '\n' : '' }`);
+    }
   })
 })
 
